d354a8b02c8137afeab66d9e8ce97688301d9132
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-26 01:20:02 +0700

    chore: init react and config all

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   devServer: {
     port: 3000,
-    watchContentBase: true,
   },
   module: {
     rules: [
@@ -25,5 +24,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
 };
