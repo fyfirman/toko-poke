5c0d794bb7b651c2060dbe449de7b6cef07948bc
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-28 16:18:13 +0700

    perf: add server to enable gzip and cache

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const expressStaticGzip = require("express-static-gzip");
+
+const app = express();
+
+app.use(
+  expressStaticGzip(path.join(__dirname, "dist"), {
+    enableBrotli: true,
+    orderPreference: ["br", "gzip"],
+    serveStatic: {
+      cacheControl: true,
+      maxAge: 31536000,
+    },
+  }),
+);
+
+app.use("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("\x1b[32m%s\x1b[0m", `App running on port ${PORT}`));
